aaba6035755c634d7fc642110d7ace3f46e2588e
Nirajsah <nirajsahy4aj@gmail.com>
2024-05-21 20:56:33 +0530

    refactor: Update middleware to use dynamic URL for redirects

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,21 @@
 import { NextResponse } from 'next/server'
 
 export default function middleware(req: { cookies: any; url: string }) {
+  const URL = 'https://coza-store-rose.vercel.app/'
   const verifyAdmin = req.cookies.get('admin-session')
   const verifyUser = req.cookies.get('coza-session')
 
   const url = req.url
 
   if (!verifyAdmin && url.includes('/admin/category')) {
-    return NextResponse.redirect('http://localhost:3000/admin')
+    return NextResponse.redirect(`${URL}/admin`)
   }
 
-  if (verifyAdmin && url === 'http://localhost:3000/admin') {
-    return NextResponse.redirect('http://localhost:3000/admin/category')
+  if (verifyAdmin && url === `${URL}/admin`) {
+    return NextResponse.redirect(`${URL}/admin/category`)
   }
 
   if (!verifyUser && url.includes('/cart')) {
-    return NextResponse.redirect('http://localhost:3000')
+    return NextResponse.redirect(`${URL}`)
   }
 }
